384ffeaf79d50d816fa894007240ed1dbe3ae3bc
iOne <rukerower@gmail.com>
2018-10-13 17:48:39 +0500

    update sf.py add new function

--- sf.py
+++ sf.py
@@ -13,7 +13,24 @@ def home(user):
 
 @app.route('/add/<x>/<y>')
 def add(x,y):
-	return 'result of summ is ' + x + y  ##ahahah int(x) + int(y)
+	return 'result of summ is ' + x + y  ##ahah int(x) + int(y)
+
+
+@app.route('/file/<file>')
+def trytoopen(file = None):
+	if file is None:                     # when will it run???
+		return 'try /file/file_name'
+	try:
+		f = open(file)
+		if file == 'sf.py':
+			r = f.read()
+			f.close()
+			return r
+		else:
+			f.close()
+			return 'File {} exist'.format(file)
+	except FileNotFoundError:
+		return 'File {} does not exist! try /file/sf.py or 1.txt'.format(file)
 
 
 if __name__ == '__main__':
